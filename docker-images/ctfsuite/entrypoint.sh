deda576ae6ff7a4a1f0b4593ea2acf08ef31b308
Yasin Ozel <yasin.ozel@metglobal.com>
2020-03-04 12:04:28 +0300

    Remove tmux and zsh. Also docker compose improved

--- docker-images/ctfsuite/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-(sysctl -p > /dev/null && echo "[#] sysctl -p is OK") || echo "[!] sysctl -p is FAILED. You may want to use docker run --privileged ..."
-ln -s /host/.zsh_history /root/.zsh_history
-exec "$@"
