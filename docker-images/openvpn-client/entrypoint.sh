e7ef15e70b079c4e0485ddbf49d41825053ecc26
Yasin Ozel <yasin.ozel@metglobal.com>
2020-03-20 22:36:29 +0300

    Redirect all UDP traffic to ctfsuite

--- docker-images/openvpn-client/entrypoint.sh
+++ docker-images/openvpn-client/entrypoint.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 iptables --table nat --append POSTROUTING --out-interface tun0 -j MASQUERADE
 iptables --append FORWARD --in-interface eth0 -j ACCEPT
 iptables -t nat -A PREROUTING -i tun0 -p tcp --dport 7789 -j DNAT --to-destination 172.28.28.4
+iptables -t nat -A PREROUTING -i tun0 -p udp -j DNAT --to-destination 172.28.28.4
 
 exec "$@"
